76f5db57b37751eb69634a6739d0ae1588da6e07
mardiana36 <kelikimardiana@gmail.com>
2024-07-13 21:29:13 +0800

    event hendler react

--- my-react-app2/src/components/Elements/input/input.jsx
+++ my-react-app2/src/components/Elements/input/input.jsx
@@ -1,7 +1,7 @@
 const Input = (params) => {
     const { type, placeholder,name } = params;
     return (
-        <input id={name} type={type} name={name} className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder={placeholder} />
+        <input id={name} required type={type} name={name} className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder={placeholder} />
     );
 }
 
--- my-react-app2/src/components/Fragments/FormLogin.jsx
+++ my-react-app2/src/components/Fragments/FormLogin.jsx
@@ -1,8 +1,15 @@
 import InputForm from "../Elements/input";
 import Button from "../Elements/Button";
 const FormLogin = () => {
+  const heandelLogin = () => {
+    event.preventDefault();
+    // menyimpan data di local storage  tidak di rekumendasikan untuk di gunakan pada login sebenarnya
+    localStorage.setItem('email', event.target.email.value);
+    localStorage.setItem('password', event.target.password.value);
+    window.location.href ="/products"
+  }
   return (
-    <form action="">
+    <form action="" onSubmit={heandelLogin}>
       <InputForm
         label="Email"
         name="email"
@@ -16,7 +23,7 @@ const FormLogin = () => {
         type="password"
       />
 
-      <Button bg="bg-blue-600 w-full">Login</Button>
+      <Button bg="bg-blue-600 w-full" type="submit">Login</Button>
     </form>
   );
 };
