bd96eb721bbb386a5984858905b790033fbec014
mardiana36 <kelikimardiana@gmail.com>
2024-07-25 14:38:33 +0800

    penerapan redux dalam program

--- my-react-app2/src/main.jsx
+++ my-react-app2/src/main.jsx
@@ -1,43 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LoginPage from './Pages/login.jsx';
-import RegisterPage from './Pages/register.jsx';
-import ErrorPage from './Pages/404.jsx';
-import ProductPage from './Pages/prodacts.jsx';
-import ProfilePage from './Pages/profile.jsx';
-import DetailProduct from './Pages/detailProduct.jsx';
+import LoginPage from "./Pages/login.jsx";
+import RegisterPage from "./Pages/register.jsx";
+import ErrorPage from "./Pages/404.jsx";
+import ProductPage from "./Pages/prodacts.jsx";
+import ProfilePage from "./Pages/profile.jsx";
+import DetailProduct from "./Pages/detailProduct.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div> Hello World</div>,
-    errorElement: <ErrorPage/>
+    errorElement: <ErrorPage />,
   },
   {
     path: "/login",
-    element: <LoginPage/>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <RegisterPage/>,
+    element: <RegisterPage />,
   },
   {
     path: "/products",
-    element: <ProductPage/>,
+    element: <ProductPage />,
   },
   {
     path: "/profile",
-    element: <ProfilePage/>
-  }, {
+    element: <ProfilePage />,
+  },
+  {
     path: "/product/:id",
-    element: <DetailProduct/>
-  }
-])
+    element: <DetailProduct />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
